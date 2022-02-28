d928e56446ca0e573efcbedd8aef9d67ea823d8b
trevisqhala <trevis.wamuthenya@qhala.com>
2022-02-28 14:14:11 +0300

    website changes based on the pdf

--- pages/index.vue
+++ pages/index.vue
@@ -9,21 +9,21 @@
         <v-card-text class="text-center font-weight-bold grey--text text-h6">
           Brought to you by Graca Machel Trust
         </v-card-text>
-        <v-card-text class="text-center mx-auto text-h3 font-weight-bold" style="width:100%; color:#FF374F;">
+        <v-card-text class="text-center mx-auto text-h3" style="width:100%; color:#FF374F;">
           A Pan- African Movement that inspires and equips female entrepreneurs to build wealth-generating businesses.
         </v-card-text>
-        <v-card-text class="text-center mx-auto" style="width:45%;">
+        <v-card-text class="text-center mx-auto montserrate-font" style="width:45%;">
           Connectiong 10,000+ women to transform their business and drive intra-Africa trade
         </v-card-text>
         <!-- two buttons for register and more about us -->
         <v-row class="my-6 mx-auto" style="width:50%">
           <v-col class="text-center">
-            <v-btn elevation="0" color="#FF374F" class="white--text" @click.prevent="registerNow">
+            <v-btn elevation="0" color="#FF374F" class="white--text poppins-font" @click.prevent="registerNow">
               Register Now
             </v-btn>
           </v-col>
           <v-col class="text-center">
-            <v-btn elevation="0" color="#FFCC18" outlined class="font-weight-bold">
+            <v-btn elevation="0" color="#FFCC18" outlined class="poppins-font">
               About Us
             </v-btn>
           </v-col>
@@ -106,5 +106,14 @@ export default {
 }
 </script>
 <style>
-
+@import url('https://fonts.googleapis.com/css2?family=DM+Serif+Display&family=Montserrat&family=Poppins&display=swap');
+  .montserrate-font{
+    font-family: 'Montserrat', sans-serif !important;
+  }
+ .dm-serif-font{
+   font-family: 'DM Serif Display', sans-serif !important;
+ }
+ .poppins-font{
+   font-family: 'Poppins', sans-serif !important;
+ }
 </style>
